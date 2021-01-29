465417b1673da4c8adb8938665cf283ec04bd69c
mbonifasi2019139 <mbonifasi-2019139@kinal.edu.gt>
2021-01-28 23:29:43 -0600

    Working in the aside

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
-let formularioLogin = document
-    .querySelector("#formulario-login")
-    .addEventListener("submit", (e) => {
-        e.preventDefault();
-        Swal.fire("Marcos Bonifasi - 2019139 - Agenda Web").then(() => {
-            window.location.href = "contactos.html";
+// ----------------------------  LOGIN ----------------------------------
+if (document.querySelector("#formulario-login")) {
+    let formularioLogin = document
+        .querySelector("#formulario-login")
+        .addEventListener("submit", (e) => {
+            e.preventDefault();
+            Swal.fire("Marcos Bonifasi - 2019139 - Agenda Web").then(() => {
+                window.location.href = "contactos.html";
+            });
         });
-    });
+}
 
-let crearCuenta = document
-    .querySelector(".campo.link")
-    .addEventListener("click", () => {
-        Swal.fire("No se ha desarrollado esta funcion");
+if (document.querySelector(".campo.link")) {
+    let crearCuenta = document
+        .querySelector(".campo.link")
+        .addEventListener("click", () => {
+            Swal.fire("No se ha desarrollado esta funcion");
+        });
+}
+
+// --------------------------- contactos ---------------------------------
+if (document.querySelector("#usuario-enlace")) {
+    let enlaceUsuario = (document.querySelector(
+        "#usuario-enlace"
+    ).onclick = () => {
+        window.location.href = "perfil.html";
     });
+}
+
+if (document.querySelector("#configuraciones")) {
+    let configuraciones = document
+        .querySelector("#configuraciones")
+        .addEventListener("click", (e) => {
+            e.preventDefault();
+            Swal.fire("No se ha desarrollado esta funcion");
+        });
+}
